c047146471ea929ec0a90e9d1ebed19c28609e13
june29 <june29.jp@gmail.com>
2013-09-16 20:03:30 +0900

    Add deploy setting

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "http://rubygems.org"
 
 gem "middleman", "~>3.1.5"
 gem "middleman-livereload", "~> 3.1.0"
+
+gem "middleman-deploy"
--- config.rb
+++ config.rb
@@ -66,3 +66,8 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = 'gh-pages'
+end
